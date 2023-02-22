a4ef199e3eaa8b227bb5df43233e8b0cc5fb625d
GitHub Action <action@github.com>
2023-02-22 06:03:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/jiaqia/androidlldb/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/jiaqia/androidlldb/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jiaqia.androidlldb.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jiaqia.androidlldb.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/jiaqia/androidlldb/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jiaqia/androidlldb/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jiaqia.androidlldb.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jiaqia.androidlldb.MyBundle
 
 class MyApplicationService {
 
